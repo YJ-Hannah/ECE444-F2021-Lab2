66d25f1d6d373dde8c82b12d6a10864b66f9de3f
Yi Jia Zhang <yijia.zhang11@gmail.com>
2021-09-25 17:53:34 -0400

    [Activity4.2] Added the email field with validator that check for the utoronto email domain

--- hello.py
+++ hello.py
@@ -4,10 +4,11 @@ from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import Required, Email
 
 class NameForm(Form):
 	name = StringField('What is your name?', validators=[Required()])
+	email = StringField('What is your UofT Email address?', validators=[Required(), Email()])
 	submit = SubmitField('Submit')
 
 app = Flask(__name__)
@@ -17,14 +18,21 @@ moment = Moment(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+	isEmail = None
 	form = NameForm()
 	if form.validate_on_submit():
 		old_name = session.get('name')
 		if old_name is not None and old_name != form.name.data:
 			flash('Looks like you have changed your name!')
+		if 'utoronto' not in form.email.data:
+			isEmail = False
+		else :
+			isEmail = True
 		session['name'] = form.name.data
+		session['email'] = form.email.data
+		session['isEmail'] = isEmail
 		return redirect(url_for('index'))
-	return render_template('index.html', form=form, name=session.get('name'))
+	return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), isEmail=session.get('isEmail'))
 
 @app.route('/user/<name>')
 def user(name):
